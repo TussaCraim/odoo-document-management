af074b6b6c4be3e763672424b72d4f3837450acb
TussaCraim <nikitaks145@gmail.com>
2025-01-06 11:20:29 +0200

    changes in __manifest__.py and security/ir.model.access.csv

--- custom_addons/document_management/__manifest__.py
+++ custom_addons/document_management/__manifest__.py
@@ -1,26 +1,19 @@
 {
     'name': 'Document Management',
     'version': '13.0.1.0.0',
+    'summary': 'Module for managing documents and books',
+    'sequence': 10,
+    'description': """This module allows to manage documents""",
     'category': 'Document Management',
     'author': 'Your Name',
-    'summary': 'Module for managing documents and books',
-    'description': """
-        This module allows to:
-        * Store documents/books information
-        * Assign responsible employees
-        * Filter documents by date and employee
-    """,
-    'depends': [
-        'base',  # basic module
-        'hr',    # module for working with employees
-    ],
+    'website': '',
+    'depends': ['base', 'hr'],
     'data': [
-        'security/ir.model.access.csv',
         'security/security.xml',
+        'security/ir.model.access.csv',
         'views/document_views.xml',
         'wizards/document_report_wizard.xml',
     ],
-    'demo': [],
     'installable': True,
     'application': True,
     'auto_install': False,
